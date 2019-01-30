8ae24ed10e933d1a7aa2a3a1dc79f79b8314e16e
ica.ikeda <ikeda@ica.jp>
2019-01-30 13:00:45 +0900

    rewrite script

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 'use strict';
 
 class scrollFunc {
-  addClass() {
-    for (var j = 0; j < document.querySelectorAll('.parallax').length; j++) {
-      let el = document.querySelectorAll('.parallax')[j];
-      let offsetTop = 0;
-      while (el) {
-        offsetTop += el.offsetTop;
-        el = el.offsetParent;
-      }
-      const top = offsetTop - window.pageYOffset;
+  constructor(target){
+    this.target = target;
+  }
 
-      if ((top - window.innerHeight / 2) < 0) {
-        document.querySelectorAll('.parallax')[j].classList.add('moved');
-      } else {
-        document.querySelectorAll('.parallax')[j].classList.remove('moved');
-      }
+  check() {
+    let el = this.target;
+    let offsetTop = 0;
+    while (el) {
+      offsetTop += el.offsetTop;
+      el = el.offsetParent;
+    }
+    const top = offsetTop - window.pageYOffset;
+
+    if ((top - window.innerHeight / 2) < 0) {
+      return true;
+    }else{
+      return false;
     }
   }
 }
 
-const scrollfunc = new scrollFunc();
-window.addEventListener('scroll', scrollfunc.addClass)
+const scrollfunc = new scrollFunc(dom);
+window.addEventListener('scroll', scrollfunc.check)
